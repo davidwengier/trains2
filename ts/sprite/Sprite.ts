7a0e4e3d42e76e2c392510d35443ae3af1161bce
Scott Holden <scott@nullops.io>
2018-06-19 17:35:05 +1000

    Fixed overdraw on Sprites

--- ts/sprite/Sprite.ts
+++ ts/sprite/Sprite.ts
@@ -3,10 +3,10 @@ module trains.play {
         private readonly canvas: HTMLCanvasElement;
         public readonly context: CanvasRenderingContext2D;
         private restored: boolean = false;
-        constructor(width: number, height: number) {
+        constructor(private width: number, private height: number) {
             this.canvas = document.createElement("canvas");
-            this.canvas.width = width;
-            this.canvas.height = height;
+            this.canvas.width = this.width + 1;
+            this.canvas.height = this.height + 1;
 
             var context = this.canvas.getContext("2d");
 
@@ -26,7 +26,7 @@ module trains.play {
                 this.context.restore();
                 this.restored = true;
             }
-            context.drawImage(this.canvas, x - 0.5, y - 0.5);
+            context.drawImage(this.canvas, 0, 0, this.width, this.height, x - 0.5, y - 0.5, this.width, this.height);
         }
     }
 }
