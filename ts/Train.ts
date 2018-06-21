4eafd5df4432f1c7f741f1c4ff489f8bb2a4f2be
Scott Holden <scott@nullops.io>
2018-06-22 09:43:39 +1000

    Cleanup

--- ts/Train.ts
+++ ts/Train.ts
@@ -68,7 +68,6 @@ export default class Train {
     }
 
     public imageReverse: number = 1;
-    //public carriage: TrainCarriage | undefined;
     public carriagePadding: number = 2;
     public nextSmoke = 0;
 
@@ -135,6 +134,7 @@ export default class Train {
                 break;
             }
         }
+        // TODO: Calculate carriage position
         /*
         if (this.carriage !== undefined) {
             this.carriage.trainSpeed = this.trainSpeed;
@@ -435,53 +435,5 @@ export default class Train {
 
     private setPaused(paused: boolean): void {
         this.isPaused = paused;
-        /*
-        if (this.carriage !== undefined) {
-            this.carriage.setPaused(paused);
-        }
-        */
     }
 }
-/*
-// tslint:disable-next-line:max-classes-per-file
-class TrainCarriage extends Train {
-
-    constructor(id: number,
-                coords: ITrainCoords,
-                renderer: TrainRenderer,
-                trainColourIndex: number,
-                gameBoard: Board,
-                cellSize: number) {
-
-        super(id, coords, renderer, trainColourIndex, "carriage", gameBoard, cellSize);
-    }
-
-    public draw(context: CanvasRenderingContext2D, translate: boolean = true): void {
-        const x = this.coords.currentX;
-        const y = this.coords.currentY;
-        const angle = Math.atan2(this.coords.previousX - x, this.coords.previousY - y);
-
-        context.save();
-
-        if (translate) {
-            context.translate(x, y);
-            context.rotate((angle * -1) + ((this.imageReverse < 0) ? Math.PI : 0));
-        } else {
-            context.translate(this.cellSize / 2, this.cellSize / 2);
-        }
-
-        this.Renderer.DrawCarriage(context, this.trainColourIndex);
-
-        context.restore();
-
-        if ((this.carriage !== undefined) && translate) {
-            this.carriage.draw(context, translate);
-            this.drawLink(context);
-        }
-    }
-
-    public drawLighting(_: CanvasRenderingContext2D): void {
-        // Do nothing
-    }
-}
-*/
